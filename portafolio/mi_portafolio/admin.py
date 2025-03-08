08d16880e980df240e9382b4863e96de9e7c1ca5
Jorge Cardona <127462388+JCARDONA1650@users.noreply.github.com>
2025-03-07 21:49:15 -0500

    correcciones de estructura de proyecto

--- portafolio/mi_portafolio/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Post, Comentario, LoginUs
-
-
-admin.site.register(Post)
-admin.site.register(Comentario)
-admin.site.register(LoginUs)
--- portafolio/settings.py
+++ portafolio/settings.py
@@ -1,6 +1,4 @@
-
-
-pathlib import Path
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
